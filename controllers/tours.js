d96d705f80470688003e404ba82862f976bb07bd
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-07 09:01:53 -0500

    feat: Added route handler which provides analytical data about tourscollection using Aggregate pipeline provided by mongodb

--- controllers/tours.js
+++ controllers/tours.js
@@ -115,3 +115,36 @@ exports.aliasTopFiveTours = (req, res, next) => {
   req.query = { limit: '5', sort: 'ratingsAverage' };
   next();
 };
+
+exports.getToursStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 1 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          totalTours: { $sum: 1 },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: -1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      err: 'failed to fetch stats',
+    });
+  }
+};
