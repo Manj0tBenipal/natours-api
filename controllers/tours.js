f4beadd80bf2c3e71d83b8c390e18c5064ebb3f1
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-08 10:37:41 -0500

    feat: USe of mongodb aggregate pipeline to get the number of tours per month in a given year

--- controllers/tours.js
+++ controllers/tours.js
@@ -144,7 +144,56 @@ exports.getToursStats = async (req, res) => {
   } catch (err) {
     res.status(500).json({
       status: 'fail',
-      err: 'failed to fetch stats',
+      err: err,
+    });
+  }
+};
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const { year } = req.params;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          count: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          month: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: { plan },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      err: err,
     });
   }
 };
