e49b0ba4424c72204066020bc5b140d79f1d592b
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-10 13:58:29 -0500

    enh: Added  a virtual Property in schema to calculate and return the duration of a tour in weeks

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,54 +1,63 @@
 const mongoose = require(`mongoose`);
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'name must be a valid string'],
-    unique: true,
+const toursSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name must be a valid string'],
+      unique: true,
+    },
+    price: {
+      type: Number,
+      required: [true, 'price must be included'],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Providing Duration is  mandatory '],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Providing difficulty is necessary'],
+    },
+    reatingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'Summary is a mandatroty field'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Cover Image is a required Field'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  price: {
-    type: Number,
-    required: [true, 'price must be included'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Providing Duration is  mandatory '],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Providing difficulty is necessary'],
-  },
-  reatingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'Summary is a mandatroty field'],
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Cover Image is a required Field'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+toursSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 const Tour = mongoose.model('Tour', toursSchema);
 module.exports = Tour;
