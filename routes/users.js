8b1ca389377d14a824bc9288b38daee79b4ab4f5
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-19 15:36:30 -0500

    feat: created a route for getting all the users in the database

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { signup, login } = require('../controllers/auth');
+const { getAllUsers } = require('../controllers/users');
+const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.route('/').get(modifyQueryToFilterObjSyntax, getAllUsers);
 module.exports = router;
