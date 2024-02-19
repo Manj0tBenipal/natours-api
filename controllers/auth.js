65b2909cafb1ad6b0cd413d766b016f0076f920e
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-19 14:12:11 -0500

    enh: successfull implementation of email and passowrd login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 
 exports.signup = async (req, res) => {
@@ -19,6 +20,52 @@ exports.signup = async (req, res) => {
       token,
       data: {
         user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'failed',
+      err: err.message,
+    });
+  }
+};
+
+/**
+ * Logging in users,
+ * confirm if the user exists,
+ * if yes: Match the passwords
+ *    a. if match, create a new JWT, sign it and send to user
+ *    b. if no match: throw error
+ * if No: return error
+ */
+exports.login = async (req, res) => {
+  try {
+    /**
+     * if email or password is not provided throw nre Error
+     */
+    if (!req.body.email || !req.body.password)
+      throw new Error('email or password not provided');
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+
+    //compare the encryped password with the one provided by user
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password,
+    );
+
+    //Error is thrown on password mismatch
+    if (!passwordMatch)
+      throw new Error('Authentication failed!. Incorrect passowrd');
+
+    // A sucess res is sent when all the auth steps are completed
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: user,
       },
     });
   } catch (err) {
