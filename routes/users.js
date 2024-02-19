0ffae2efb5b68f7252f67c48279fff0bd87d7736
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-18 22:44:56 -0500

    refactor: removed unnecessary code and added a  route for user signup

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,8 @@
 const express = require('express');
+const { signup } = require('../controllers/auth');
 
 const router = express.Router();
-const getUsers = (req, res) => {
-  res.status(404).json({ status: 'failed', message: 'Item not found' });
-};
-const getUserById = (req, res) => {
-  res.status(404).json({ status: 'failed', message: 'Item not found' });
-};
-const addUser = (req, res) => {
-  res.status(404).json({ status: 'failed', message: 'Item not found' });
-};
-const updateUser = (req, res) => {
-  res.status(404).json({ status: 'failed', message: 'Item not found' });
-};
-const deleteUser = (req, res) => {
-  res.status(404).json({ status: 'failed', message: 'Item not found' });
-};
-router.route('/').get(getUsers).post(addUser);
-router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
+
+router.post('/signup', signup);
+
 module.exports = router;
