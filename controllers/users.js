5fde1dd7ea744296b11648b4f571a9df7ac8a018
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-19 23:15:10 -0500

    feat: updateUser route handler implemented

--- controllers/users.js
+++ controllers/users.js
@@ -16,3 +16,26 @@ exports.getAllUsers = async (req, res) => {
     });
   }
 };
+exports.updateUser = async (req, res) => {
+  try {
+    const { params, body } = req;
+    if (!params.id) throw new Error('Provide a userID');
+    //future enhancement will sanitize the incoming object to filter unnecessary fields
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: params.id },
+      { $set: { ...body } },
+      { new: true },
+    );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'failed',
+      err,
+    });
+  }
+};
