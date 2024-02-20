8ed7715dd19625bcfe6e93076cd90760e7f6be18
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-19 23:15:46 -0500

    feat: route for updating user added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signup, login, isLoggedIn } = require('../controllers/auth');
-const { getAllUsers } = require('../controllers/users');
+const { getAllUsers, updateUser } = require('../controllers/users');
 const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
 const router = express.Router();
@@ -8,4 +8,8 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.route('/').get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers);
+
+//This route for now does not ask user for their previous credentials
+//It is being developed temporarily to test if  pre-save hooks change lastPasswordChange in database
+router.route('/:id').patch(updateUser);
 module.exports = router;
