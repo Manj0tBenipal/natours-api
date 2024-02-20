3fccfa4fe28bce1d42e0e730bd6b67a9eada7f09
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-19 21:07:01 -0500

    feat: Added middleware for protected route GET /users which verifies if the user is logged in using JWT

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, login } = require('../controllers/auth');
+const { signup, login, isLoggedIn } = require('../controllers/auth');
 const { getAllUsers } = require('../controllers/users');
 const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
@@ -7,5 +7,5 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.route('/').get(modifyQueryToFilterObjSyntax, getAllUsers);
+router.route('/').get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers);
 module.exports = router;
