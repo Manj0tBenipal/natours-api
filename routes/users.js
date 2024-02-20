51a750d6ff781177af515f99622b3a75ee56a3e4
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-20 12:18:02 -0500

    feat: implemented authorization based on user roles on DELETE /user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { signup, login, isLoggedIn } = require('../controllers/auth');
+const {
+  signup,
+  login,
+  isLoggedIn,
+  allowAccessTo,
+} = require('../controllers/auth');
 const { getAllUsers, updateUser, deleteUser } = require('../controllers/users');
 const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
@@ -12,5 +17,8 @@ router.route('/').get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers);
 //This Update route for now does not ask user for their previous credentials
 //It is being developed temporarily to test if  pre-save hooks change lastPasswordChange in database
 //Future enhancement to the delete route will include checking the roles of the users before giving access to the route
-router.route('/:id').patch(updateUser).delete(isLoggedIn, deleteUser);
+router
+  .route('/:id')
+  .patch(updateUser)
+  .delete(isLoggedIn, allowAccessTo('admin', 'lead-guide'), deleteUser);
 module.exports = router;
