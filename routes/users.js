2f7c7f34ba90ff55cddbf62f13ffea0aebcc743b
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-20 11:32:48 -0500

    feat: implemented route for deleteing the users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signup, login, isLoggedIn } = require('../controllers/auth');
-const { getAllUsers, updateUser } = require('../controllers/users');
+const { getAllUsers, updateUser, deleteUser } = require('../controllers/users');
 const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
 const router = express.Router();
@@ -9,7 +9,8 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.route('/').get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers);
 
-//This route for now does not ask user for their previous credentials
+//This Update route for now does not ask user for their previous credentials
 //It is being developed temporarily to test if  pre-save hooks change lastPasswordChange in database
-router.route('/:id').patch(updateUser);
+//Future enhancement to the delete route will include checking the roles of the users before giving access to the route
+router.route('/:id').patch(updateUser).delete(isLoggedIn, deleteUser);
 module.exports = router;
