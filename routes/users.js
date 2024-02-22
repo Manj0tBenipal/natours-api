51e5750816c6dced34a1e95afefb59f44d2640cc
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-22 10:20:54 -0500

    feat: implemented new route handler [requires a logged in user] for the user to update user details other than password(name, email)

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,12 @@ const {
   resetPassword,
   changePassword,
 } = require('../controllers/auth');
-const { getAllUsers, updateUser, deleteUser } = require('../controllers/users');
+const {
+  getAllUsers,
+  //  updateUser,
+  deleteUser,
+  updateMe,
+} = require('../controllers/users');
 const { modifyQueryToFilterObjSyntax } = require('../utils/functions');
 
 const router = express.Router();
@@ -18,12 +23,15 @@ router.post('/login', login);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 router.post('/change-password', isLoggedIn, changePassword);
-router.route('/').get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers);
+router
+  .route('/')
+  .get(isLoggedIn, modifyQueryToFilterObjSyntax, getAllUsers)
+  .patch(isLoggedIn, updateMe);
 //This Update route for now does not ask user for their previous credentials
 //It is being developed temporarily to test if  pre-save hooks change lastPasswordChange in database
 //Future enhancement to the delete route will include checking the roles of the users before giving access to the route
 router
   .route('/:id')
-  .patch(updateUser)
+  // .patch(allowAccessTo('admin'), updateUser)
   .delete(isLoggedIn, allowAccessTo('admin'), deleteUser);
 module.exports = router;
