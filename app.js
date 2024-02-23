27be9978a5c1e0d3679bbcbdcfd72def90ccecd7
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-23 10:35:00 -0500

    feat: Implemented rate limiting to /api route

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 
 const tourRouter = require(`${__dirname}/routes/tours.js`);
 
 const userRouter = require(`${__dirname}/routes/users.js`);
-
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request!',
+});
 const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+app.use('/api', limiter);
 app.use(express.json());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
