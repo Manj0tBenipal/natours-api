1eac3a6ddaeab133a27e0419ab69dab4caae83ce
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-26 11:37:02 -0500

    enh: created reviews controller with get and post routes to fetch and add new reviews

---
+++ controllers/review.js
@@ -0,0 +1,45 @@
+const Review = require('../models/reviewModel');
+const { filterObject } = require('../utils/functions');
+
+exports.getAllReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.status(200).json({
+      status: 'success',
+      data: {
+        reviews: reviews,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'failed',
+      err: err.message,
+    });
+  }
+};
+exports.addReview = async (req, res) => {
+  try {
+    const allowedKeys = ['rating', 'text', 'userId', 'tourId'];
+
+    //All the keys: value pairs that are not in the array are deleted from the object
+    const filteredObject = filterObject(
+      //assign the value of user._id which is generated while
+      //authenticating user up in the middleware stack to userId to be used in newReview document
+      { ...req.body, userId: req.user._id },
+      allowedKeys,
+    );
+
+    const newReview = await Review.create(filteredObject);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        newReview: newReview,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'failed',
+      err: err.message,
+    });
+  }
+};
