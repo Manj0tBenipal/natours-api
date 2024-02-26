274f8357b1703d81a1e1c6544ea9a41b690af8fc
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-26 09:53:20 -0500

    enh: created Review model to faciliate having reviews for tours

---
+++ models/reviewModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    maxLength: 300,
+    minLength: 20,
+  },
+  likes: Number,
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    reference: 'User',
+  },
+  tourId: {
+    type: mongoose.Schema.ObjectId,
+    reference: 'Tour',
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: [true, 'Rating is required'],
+  },
+  createdAt: Date,
+});
+
+const Review = mongoose.model(reviewSchema);
+module.exports = Review;
