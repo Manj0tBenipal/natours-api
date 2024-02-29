9d207d668c4740b2a0b799ffa325154dd09b16c2
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-29 12:31:23 -0500

    enh: created and addedaddTourId to query to support use of APIFEatures in GET /tours/<tourId>/reviews route

--- routes/review.js
+++ routes/review.js
@@ -4,13 +4,15 @@ const {
   addReview,
   getReviewById,
   addTourAndUserId,
+
+  addTourIdToQuery,
 } = require('../controllers/review');
 const { isLoggedIn, allowAccessTo } = require('../controllers/auth');
 
 const router = express.Router({ mergeParams: true });
 router
   .route('/')
-  .get(getAllReviews)
+  .get(addTourIdToQuery, getAllReviews)
   //addTourAndUserId adds userId from req.user to req.body.userId
   //and if tourId is not provided in req.body, it assigns it from req.params
   //Thi is done to support adding review using nested routed and normal POST /reviews route
