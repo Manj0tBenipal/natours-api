6bfeee0863fa175b5a4dfe35b466b93d27c3108f
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-29 10:44:39 -0500

    enh: added addUserAndTourId in the middleware stack when creating new resource

--- routes/review.js
+++ routes/review.js
@@ -3,6 +3,7 @@ const {
   getAllReviews,
   addReview,
   getReviewById,
+  addTourAndUserId,
 } = require('../controllers/review');
 const { isLoggedIn, allowAccessTo } = require('../controllers/auth');
 
@@ -10,6 +11,9 @@ const router = express.Router({ mergeParams: true });
 router
   .route('/')
   .get(getAllReviews)
-  .post(isLoggedIn, allowAccessTo('customer'), addReview);
+  //addTourAndUserId adds userId from req.user to req.body.userId
+  //and if tourId is not provided in req.body, it assigns it from req.params
+  //Thi is done to support adding review using nested routed and normal POST /reviews route
+  .post(isLoggedIn, allowAccessTo('customer'), addTourAndUserId, addReview);
 router.route('/:id').get(getReviewById);
 module.exports = router;
