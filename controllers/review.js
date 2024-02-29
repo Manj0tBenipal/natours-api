ad697d90472b8156e651689a1a0c777aad4efaec
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-29 12:35:23 -0500

    enh: created addTourIdToQuery to support use of APIFeatures on /tours/<tourid>/reviews route and used getResources on the same route

--- controllers/review.js
+++ controllers/review.js
@@ -1,30 +1,33 @@
 const Review = require('../models/reviewModel');
-const { getResourceById, createResource } = require('./handlerFactory');
+const {
+  getResourceById,
+  createResource,
+  getResources,
+} = require('./handlerFactory');
 
-exports.getAllReviews = async (req, res) => {
-  //allow nested routes {/tours/:id/reviews}
-  const tourIdFromReq = req.body.tourId;
-  const tourIdFromParams = req.params.tourId;
-  const tourId = tourIdFromReq !== undefined ? tourIdFromReq : tourIdFromParams;
-  try {
-    const reviews = await Review.find({ tourId: tourId });
-    res.status(200).json({
-      status: 'success',
-      data: {
-        reviews: reviews,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'failed',
-      err: err.message,
-    });
-  }
-};
+exports.getAllReviews = getResources(Review);
 exports.addTourAndUserId = (req, res, next) => {
   const tourIdFromParams = req.params.tourId;
   if (!req.body.tourId) req.body.tourId = tourIdFromParams;
   req.body.userId = req.user._id;
+  next();
+};
+
+/**
+ * This function is a middleware  which retrieves tourId from req.params
+ * or req.body and saves its value to req.query.tourId.
+ * This is done in order to support the use of APIFeatures class which allows pagination
+ * and filtering
+ *
+ * The tourId in req.query is used in Model.find({...req.query}) which returns reviews
+ * of a selected tour but with features such as advanced filtering
+ */
+exports.addTourIdToQuery = (req, res, next) => {
+  //allow nested routes {/tours/:id/reviews}
+  const tourIdFromReq = req.body.tourId;
+  const tourIdFromParams = req.params.tourId;
+  const tourId = tourIdFromReq !== undefined ? tourIdFromReq : tourIdFromParams;
+  req.query.tourId = tourId;
   next();
 };
 exports.addReview = createResource(Review, [
