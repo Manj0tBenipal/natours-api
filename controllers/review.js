b6c732386e480035f0b265c11822a35b7c4fb892
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-02-29 10:39:48 -0500

    enh: added a middleware to add tour and userId in req.body to allow use of createResouce from factoryHandler to create a new Review

--- controllers/review.js
+++ controllers/review.js
@@ -1,6 +1,5 @@
 const Review = require('../models/reviewModel');
-const { filterObject } = require('../utils/functions');
-const { getResourceById } = require('./handlerFactory');
+const { getResourceById, createResource } = require('./handlerFactory');
 
 exports.getAllReviews = async (req, res) => {
   //allow nested routes {/tours/:id/reviews}
@@ -22,35 +21,11 @@ exports.getAllReviews = async (req, res) => {
     });
   }
 };
-exports.addReview = async (req, res) => {
-  try {
-    //allow nested routes {/tours/:id/reviews}
-    const tourIdFromReq = req.body.tourId;
-    const tourIdFromParams = req.params.tourId;
-    const tourId =
-      tourIdFromReq !== undefined ? tourIdFromReq : tourIdFromParams;
-    const allowedKeys = ['rating', 'text', 'userId', 'tourId'];
-
-    //All the keys: value pairs that are not in the array are deleted from the object
-    const filteredObject = filterObject(
-      //assign the value of user._id which is generated while
-      //authenticating user up in the middleware stack to userId to be used in newReview document
-      { ...req.body, userId: req.user._id, tourId: tourId },
-      allowedKeys,
-    );
-
-    const newReview = await Review.create(filteredObject);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        newReview: newReview,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'failed',
-      err: err.message,
-    });
-  }
+exports.addTourAndUserId = (req, res, next) => {
+  const tourIdFromParams = req.params.tourId;
+  if (!req.body.tourId) req.body.tourId = tourIdFromParams;
+  req.body.userId = req.user._id;
+  next();
 };
+
 exports.getReviewById = getResourceById(Review);
