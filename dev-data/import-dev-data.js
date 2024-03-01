a347c5d8c9153160ac3f44e3c97680d61b3c3039
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-01 06:01:28 -0500

    imported reviews and user data into the database

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 
 const Tour = require('../models/tourModel');
+const Review = require('../models/reviewModel');
+const User = require('../models/userModel');
 
 const db = process.env.DB_URL.replace('PASSWORD', process.env.DB_PASS).replace(
   'USER',
@@ -12,13 +14,21 @@ const db = process.env.DB_URL.replace('PASSWORD', process.env.DB_PASS).replace(
 );
 mongoose.connect(db);
 
-const data = JSON.parse(
-  fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8'),
+const data = JSON.parse(fs.readFileSync('./dev-data/data/tours.json', 'utf-8'));
+const users = JSON.parse(
+  fs.readFileSync('./dev-data/data/users.json', 'utf-8'),
+);
+const reviews = JSON.parse(
+  fs.readFileSync('./dev-data/data/reviews.json', 'utf-8'),
 );
 const syncDataWithDb = async () => {
   try {
     await Tour.deleteMany();
     await Tour.create(data);
+    await Review.deleteMany();
+    await Review.create(reviews, { validateBeforeSave: false });
+    await User.deleteMany();
+    await User.create(users, { validateBeforeSave: false });
   } catch (err) {
     console.log(err);
   }
