b16b52c02fa1dc0c165defd2361c98589191ddf0
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-03 13:25:04 -0500

    enh: Added new global error handling middleware used to return an error response

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const tourRouter = require(`${__dirname}/routes/tours.js`);
 const reviewRouter = require(`${__dirname}/routes/review.js`);
 const userRouter = require(`${__dirname}/routes/users.js`);
+
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -41,6 +42,15 @@ app.all('*', (req, res) => {
   res.status(404).json({
     status: 'failed',
     err: 'This tour does not exist',
+  });
+});
+app.use((err, req, res, next) => {
+  let { statusCode, message } = err;
+  if (!statusCode) statusCode = 500;
+  if (!message) message = 'An unknown error has occured';
+  res.status(statusCode).json({
+    status: 'fail',
+    message: message,
   });
 });
 module.exports = app;
