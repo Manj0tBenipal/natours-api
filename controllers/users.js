76aec304db66dcc1e9f8d08318100d7f0b920aa5
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-03 13:57:39 -0500

    feat: implemented error handling with catchAsync in all route handlers which triggers global error handler middleware and sends a response to client with appropriate status code and message

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel');
-const { signJWT } = require('../utils/functions');
+const AppError = require('../utils/AppError');
+
+const { signJWT, catchAsync } = require('../utils/functions');
 const {
   deleteResourceById,
   updateResource,
@@ -15,117 +17,96 @@ exports.addUserIdToParams = (req, res, next) => {
   next();
 };
 exports.getMe = getResourceById(User);
-exports.updateMe = async (req, res) => {
-  try {
-    //User needs to successfully pass isLoggedIn middleware to update name and email
-    //isLoggedIn then will add a user Object to the query
-    const { user } = req;
+exports.updateMe = catchAsync(async (req, res) => {
+  //User needs to successfully pass isLoggedIn middleware to update name and email
+  //isLoggedIn then will add a user Object to the query
+  const { user } = req;
 
-    //only allow updating name and email
-    //password is updated with a different route handler
-    const { name, email } = req.body;
+  //only allow updating name and email
+  //password is updated with a different route handler
+  const { name, email } = req.body;
 
-    //either name or email should be in the req body
-    if (!name && !email) throw new Error('Insufficient Data!');
-    user.name = name;
-    user.email = email;
-    await user.save();
-    res.status(200).json({
-      status: 'success',
-      data: {
-        updatedUser: user._id,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'failed',
-      err: { ...err },
-    });
-  }
-};
+  //either name or email should be in the req body
+  if (!name && !email) throw new AppError('Insufficient Data!', 400);
+  user.name = name;
+  user.email = email;
+  await user.save();
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser: user._id,
+    },
+  });
+});
 
-exports.deactivateAccount = async (req, res) => {
-  try {
-    //isLoggedIn middleware needs to be executed successfully
-    //to access this route handler. isLoggedIn also adds the user object
-    //of the user currently logged in to the reqest object
+exports.deactivateAccount = catchAsync(async (req, res) => {
+  //isLoggedIn middleware needs to be executed successfully
+  //to access this route handler. isLoggedIn also adds the user object
+  //of the user currently logged in to the reqest object
+  const user = await User.findById(req.user._id).select('+password');
 
-    const user = await User.findById(req.user._id).select('+password');
-    //deactivating account requires user to send currentPassword inthe request body
-    const { password } = req.body;
-    if (!password)
-      throw new Error('Password is required to deactivate the account!');
+  //deactivating account requires user to send currentPassword inthe request body
+  const { password } = req.body;
+  if (!password)
+    throw new AppError('Password is required to deactivate the account!', 401);
 
-    //check if the user has entered the correct password
-    if (!(await user.passwordMatch(password, user.password)))
-      throw new Error('Authentication failed! Incorrect Password!');
-    //if user is authenticated successfully deactivate the account
+  //check if the user has entered the correct password
+  if (!(await user.passwordMatch(password, user.password)))
+    throw new AppError('Authentication failed! Incorrect Password!', 401);
+  //if user is authenticated successfully deactivate the account
 
-    user.active = false;
-    //Since the value of activate is set to false by the routeHandler
-    //not the user, validation is not required
-    await user.save({
-      validateBeforeSave: false,
-    });
+  user.active = false;
+  //Since the value of activate is set to false by the routeHandler
+  //not the user, validation is not required
+  await user.save({
+    validateBeforeSave: false,
+  });
 
-    res.status(200).json({
-      status: 'failed',
-      data: {
-        deactivateUser: user._id,
-      },
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: 'failed',
-      err: err.message,
-    });
-  }
-};
-exports.activateAccount = async (req, res) => {
-  try {
-    //retrieve credentials from req.body
-    const { email, password } = req.body;
+  res.status(200).json({
+    status: 'failed',
+    data: {
+      deactivateUser: user._id,
+    },
+  });
+});
+exports.activateAccount = catchAsync(async (req, res) => {
+  //retrieve credentials from req.body
+  const { email, password } = req.body;
 
-    //check if both email and password are provided in req.body
-    if (!email || !password) throw new Error('Insufficient Data!');
+  //check if both email and password are provided in req.body
+  if (!email || !password) throw new AppError('Insufficient Data!', 400);
 
-    //find the user based on email address
-    /**
-     * NOTE: there is a middleware which executes before each find query
-     */
-    const user = await User.findOne({
-      email: email,
-      includeInactive: true,
-    }).select('+password +active');
+  //find the user based on email address
+  /**
+   * NOTE: there is a middleware which executes before each find query
+   */
+  const user = await User.findOne({
+    email: email,
+    includeInactive: true,
+  }).select('+password +active');
 
-    //check if with same email is found
-    if (!user) throw new Error('Invalid Username or password');
+  //check if with same email is found
+  if (!user) throw new AppError('Invalid Username or password', 401);
 
-    //verify the credentials
-    if (!(await user.passwordMatch(password, user.password)))
-      throw new Error('Invalid Username or password');
-    if (user.active)
-      throw new Error('Your account is already active! Please login');
-    //change status of account from inactive to active
-    user.active = true;
+  //verify the credentials
+  if (!(await user.passwordMatch(password, user.password)))
+    throw new AppError('Invalid Username or password', 401);
+  if (user.active)
+    throw new AppError('Your account is already active! Please login', 400);
+  //change status of account from inactive to active
+  user.active = true;
 
-    //save changes
-    await user.save({
-      validateBeforeSave: false,
-    });
+  //save changes
+  await user.save({
+    validateBeforeSave: false,
+  });
 
-    //create a new JWT and attach it to the res object as a cookie
-    signJWT(user._id, res);
-    res.status(200).json({
-      status: 'success',
-      data: {
-        activatedUser: user._id,
-      },
-    });
-  } catch (err) {
-    res.status(401).json({
-      status: 'failed',
-      err: err.message,
-    });
-  }
-};
+  //create a new JWT and attach it to the res object as a cookie
+  signJWT(user._id, res);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      activatedUser: user._id,
+    },
+  });
+});
