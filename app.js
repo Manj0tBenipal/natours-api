f353a49de07337e9fbeaf2ff35117e9f2db31c81
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-03 17:35:58 -0500

    enh: added a welcome message for GET request at root url

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ app.use(express.json());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message:
+      'Welcome to natours-api 🎉. Please check the documentation at:https://documenter.getpostman.com/view/32620000/2sA2xb6vto#ca5ab2ae-773f-4346-8e30-0ac79b9ecdbf',
+  });
+});
 app.all('*', (req, res) => {
   res.status(404).json({
     status: 'failed',
