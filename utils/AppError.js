90a4d2a4ee249179bdb176cd51eb096202c323c2
Manj0tBenipal <manjotbenipal00001@gmail.com>
2024-03-03 14:33:41 -0500

    refactor: Added documentation for class

--- utils/AppError.js
+++ utils/AppError.js
@@ -1,3 +1,9 @@
+/**
+ * This class provides some extra fields to work with status codes while handling errors
+ * When an App error is thrown, it is passed down to global error handling middleware using next(err)
+ * or throwing err inside of catchAsync function.
+ * @see catchAsync
+ */
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
